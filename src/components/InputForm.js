5f50d8b5ada8d11c10115c5ef42da2c29d51c7e1
tania <tanakorchynska@gmail.com>
2023-08-15 19:39:05 -0400

    made the input responsive

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,11 +4,16 @@ import "./InputForm.css";
 const InputForm = () => {
   return (
     <div>
-          <p className="f3 center">{"This is the blablabla"}</p>
-          <div className="center searchline">
-              <input type="text" className="f4 pa2 w-70 center"/>
-              <button className=" center grow f4 w-25 link ph3 pv2 dib br1 black">Detect</button>
-          </div>
+      <p className="f3 center">{"This is the blablabla"}</p>
+      <div className="center searchline">
+        <input type="text" className="f4 pa2 w-70 center" />
+        <button
+          className="center grow f4 w-25 link ph3 pv2 dib br1 black"
+          id="btnDetect"
+        >
+          Detect
+        </button>
+      </div>
     </div>
   );
 };
