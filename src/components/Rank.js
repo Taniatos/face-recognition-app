dc014404f4788af654ae29ac57756a59569eea85
tania <tanakorchynska@gmail.com>
2023-08-25 10:58:05 -0400

    Fixed Rank style

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Rank.css";
 
 export default function Rank({ name, entries }) {
   return (
-    <div>
-      <div className="white f3">
+    <div className="rank-style">
+      <div className="black f3">
         {`${name}, your current entry count is...`}
       </div>
-      <div className="white f1">{entries}</div>
+      <div className="black f1">{entries}</div>
     </div>
   );
 }
