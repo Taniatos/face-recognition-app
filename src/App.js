c8389f3bfcd81ffc2bf6d908bdd5e15b0ce5d59c
tania <tanakorchynska@gmail.com>
2023-09-01 14:46:18 -0400

    Discarded recent changes

--- src/App.js
+++ src/App.js
@@ -36,22 +36,65 @@ function App() {
     setInput(event.target.value);
   };
 
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
   const displayFaceBox = (box) => {
     setBox(box);
   };
 
   const onButtonSubmit = () => {
-    // Send the input image URL to your back-end
-    fetch("http://localhost:3000/imageurl", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        input: input,
-      }),
-    })
+    const PAT = "269ccf548ec042d1bb9113647e2f2491";
+    const USER_ID = "taniatos";
+    const APP_ID = "visio-quest-app";
+    const MODEL_ID = "face-detection";
+
+    const returnClarifaiRequestOptions = (imageUrl) => {
+      const IMAGE_URL = imageUrl;
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: "Key " + PAT,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_app_id: {
+            user_id: USER_ID,
+            app_id: APP_ID,
+          },
+          inputs: [
+            {
+              data: {
+                image: {
+                  url: IMAGE_URL,
+                },
+              },
+            },
+          ],
+        }),
+      };
+      return requestOptions;
+    };
+
+    fetch(
+      `https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`,
+      returnClarifaiRequestOptions(input)
+    )
       .then((response) => response.json())
       .then((data) => {
-        displayFaceBox(data);
+        displayFaceBox(calculateFaceLocation(data));
         if (data) {
           fetch("http://localhost:3000/image", {
             method: "put",
