390d97a0504b78a66f486dd01274b4366811108f
tania <tanakorchynska@gmail.com>
2023-09-01 13:47:28 -0400

    Hid the API key to the back end

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import ParticlesBg from "particles-bg";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-import Rank from "./components/Rank/Rank"; 
+import Rank from "./components/Rank/Rank";
 import InputForm from "./components/InputForm/InputForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import SignIn from "./components/SignIn/SignIn";
@@ -13,98 +13,52 @@ function App() {
   const [input, setInput] = useState("");
   const [box, setBox] = useState({});
   const [route, setRoute] = useState("signin");
-  
-  const [user, setUser] = useState({ 
-    id: '',
-    name: '',
-    email: '',
+
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
+    joined: "",
   });
 
-  const loadUser = (data) => { 
+  const loadUser = (data) => {
     setUser({
       id: data.id,
       name: data.name,
       email: data.email,
       entries: data.entries,
-      joined: data.joined
-    })
-  }
+      joined: data.joined,
+    });
+  };
 
   const onInputChange = (event) => {
     setInput(event.target.value);
   };
 
-  const calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById("inputImage");
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
-  };
-
-  const displayFaceBox = (box) => {
-    setBox(box);
-  };
-
   const onButtonSubmit = () => {
-    const PAT = "ee6ae3e6775240099bf9d138ed18ef89";
-    const USER_ID = "taniatos";
-    const APP_ID = "visio-quest-app";
-    const MODEL_ID = "face-detection";
-
-    const returnClarifaiRequestOptions = (imageUrl) => {
-      const IMAGE_URL = imageUrl;
-      const requestOptions = {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          Authorization: "Key " + PAT,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          user_app_id: {
-            user_id: USER_ID,
-            app_id: APP_ID,
-          },
-          inputs: [
-            {
-              data: {
-                image: {
-                  url: IMAGE_URL,
-                },
-              },
-            },
-          ],
-        }),
-      };
-      return requestOptions;
-    };
-
-    fetch(
-      `https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`,
-      returnClarifaiRequestOptions(input)
-    )
+    // Send the input image URL to your back-end
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: input,
+      }),
+    })
       .then((response) => response.json())
       .then((data) => {
-        displayFaceBox(calculateFaceLocation(data));
+        displayFaceBox(data);
         if (data) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: user.id
-            })
+              id: user.id,
+            }),
           })
-            .then(response => response.json())
-            .then(count => {
-              setUser(prevUser => ({ ...prevUser, entries: count }));
+            .then((response) => response.json())
+            .then((count) => {
+              setUser((prevUser) => ({ ...prevUser, entries: count }));
             });
         }
       })
@@ -118,7 +72,7 @@ function App() {
       <ParticlesBg type="cobweb" num={80} bg={true} />
       <Navigation onRouteChange={setRoute} route={route} />
       {route === "signin" ? (
-        <SignIn onRouteChange={setRoute} loadUser={loadUser} /> 
+        <SignIn onRouteChange={setRoute} loadUser={loadUser} />
       ) : route === "register" ? (
         <Register onRouteChange={setRoute} loadUser={loadUser} />
       ) : (
